2b3cdf7365abd58d957b8a1afc2d20abadc36523
Bowei Wang <wang.bowe@husky.neu.edu>
2016-04-15 17:09:15 -0400

    startSpingSecurity

--- intelijworkspace/finalproject6250/src/main/java/service/MyUserDetailsService.java
+++ intelijworkspace/finalproject6250/src/main/java/service/MyUserDetailsService.java
@@ -1,52 +1,54 @@
-package service;
-
-import Dao.CombinedAccountDao;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import proj.AccountPkg.CombinedAccount;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-/**
- * Created by kym-1992 on 3/30/16.
- */
-@Service("userDetailsService")
-public class MyUserDetailsService implements UserDetailsService {
-
-    CombinedAccountDao combinedAccountDao;
-
-    @Transactional(readOnly = true)
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        CombinedAccount combinedAccount = combinedAccountDao.get(userName);
-        List<GrantedAuthority> authorities = buildUserAuthority(combinedAccount);
-
-        return buildUserForAuthentication(combinedAccount, authorities);
-    }
-
-    private User buildUserForAuthentication(CombinedAccount combinedAccount, List<GrantedAuthority> authorities) {
-        return new User(combinedAccount.getUserName(), combinedAccount.getPassword(), true, true, true, true, authorities);
-
-    }
-
-    private List<GrantedAuthority> buildUserAuthority(CombinedAccount combinedAccount) {
-
-        Set<GrantedAuthority> setAuths = new HashSet<GrantedAuthority>();
-
-        // Build user's authorities
-        setAuths.add(new SimpleGrantedAuthority(combinedAccount.getAccountType().getValue()));
-
-        List<GrantedAuthority> Result = new ArrayList<GrantedAuthority>(setAuths);
-
-        return Result;
-    }
-}
-
+//package service;
+//
+//import Dao.CombinedAccountDao;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//import proj.AccountPkg.CombinedAccount;
+//
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.Set;
+//
+///**
+// * Created by kym-1992 on 3/30/16.
+// */
+//@Service("userDetailsService")
+//public class MyUserDetailsService implements UserDetailsService {
+//
+//    @Autowired
+//    CombinedAccountDao combinedAccountDao;
+//
+//    @Transactional(readOnly = true)
+//    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+//        CombinedAccount combinedAccount = combinedAccountDao.get(userName);
+//        List<GrantedAuthority> authorities = buildUserAuthority(combinedAccount);
+//
+//        return buildUserForAuthentication(combinedAccount, authorities);
+//    }
+//
+//    private User buildUserForAuthentication(CombinedAccount combinedAccount, List<GrantedAuthority> authorities) {
+//        return new User(combinedAccount.getUserName(), combinedAccount.getPassword(), true, true, true, true, authorities);
+//
+//    }
+//
+//    private List<GrantedAuthority> buildUserAuthority(CombinedAccount combinedAccount) {
+//
+//        Set<GrantedAuthority> setAuths = new HashSet<GrantedAuthority>();
+//
+//        // Build user's authorities
+//        setAuths.add(new SimpleGrantedAuthority(combinedAccount.getAccountType().getValue()));
+//
+//        List<GrantedAuthority> Result = new ArrayList<GrantedAuthority>(setAuths);
+//
+//        return Result;
+//    }
+//}
+//
--- intelijworkspace/finalproject6250/src/main/java/service/SecurityWebApplicationInitializer.java
+++ intelijworkspace/finalproject6250/src/main/java/service/SecurityWebApplicationInitializer.java
@@ -1,12 +1,28 @@
-package service;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * Created by kym-1992 on 4/2/16.
- */
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-    public SecurityWebApplicationInitializer() {
-        super(SecurityConfig.class);
-    }
-}
+//package service;
+//
+//import configFiles.appConfig;
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+///**
+// * Created by kym-1992 on 4/2/16.
+// */
+//public class SecurityWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+////    public SecurityWebApplicationInitializer() {
+////        super(SecurityConfig.class);
+////    }
+//
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class[] {SecurityConfig.class};
+//    }
+//
+//    @Override
+//    protected Class< ? >[] getServletConfigClasses() {
+//        return new Class[] { appConfig.class };
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[] { "/" };
+//    }
+//}
